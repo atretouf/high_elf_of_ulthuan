838622477508c054b2f5b1b81d9dcd9f9e00ba55
atretouf <atreto@ufl.edu>
2020-04-06 06:24:50 +0000

    First App

---
+++ my-app/build/precache-manifest.fb3689a78001f14dde8d5cf515fad9fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5eebfffcd49f29623f0ed4f55879266",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85fe29ee0d0437116f16",
+    "url": "/static/css/main.3448edf6.chunk.css"
+  },
+  {
+    "revision": "71229c1c2a04f77a7f38",
+    "url": "/static/js/2.4b0bffa4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b0bffa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85fe29ee0d0437116f16",
+    "url": "/static/js/main.57a1743e.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "458076dd583114c3065a6757fbe4320c",
+    "url": "/static/media/helf.458076dd.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
